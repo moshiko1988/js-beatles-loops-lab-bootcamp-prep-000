893c3c1bcbcf34cf2571f28fd87c6e8cd1c57d11
Moshe Srur <moshiko1988@gmail.com>
2018-04-08 03:51:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function theBeatlesPlay (array1, array2){
+function theBeatlesPlay` (array1, array2){
   var arr = [];
   for (var i = 0; i < array1.length; i++){
     arr.push(`${array1[i]} plays ${array2[i]}`);
