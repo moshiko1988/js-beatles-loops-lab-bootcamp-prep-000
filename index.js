cde6b0c8178f11525610a774ad59a9a41e05dba8
Moshe Srur <moshiko1988@gmail.com>
2018-04-08 03:53:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-function theBeatlesPlay (musicians, array2){
+function theBeatlesPlay (musicians, instruments){
   var arr = [];
   for (var i = 0; i < musicians.length; i++){
-    arr.push(`${musicians[i]} plays ${array2[i]}`);
+    arr.push(`${musicians[i]} plays ${instruments[i]}`);
   }
   return arr
 }
